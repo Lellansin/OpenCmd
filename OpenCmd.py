55912a3efca059ac2a1e1eecb6ac646b3cc936ad
Lellansin <lellansin@gmail.com>
2013-08-10 10:39:10 +0800

    little change

--- OpenCmd.py
+++ OpenCmd.py
@@ -1,31 +1,30 @@
 
 import sublime, sublime_plugin
-import os, sys;
-
+import os;
 
 class OpenCmdCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 
 		cwd = os.getcwd();
 		path = self.view.file_name();
-		platform = sys.platform;
-
-		if(platform[0:3] == 'win'):
+		platform = sublime.platform();
+		
+		if(platform == 'windows'):
 			# get file directory
 			path = path[0:path.rfind('\\')];
 			# cd directory and start a cmd
-			command = path[0:2] + ' && ' + 'cd "' + path + '" && start cmd"'
+			command = path[0:2] + ' && ' + 'cd "' + path + '" && start cmd'
+			# sublime.message_dialog(command)
 			os.system(command)
-		elif(platform[0:5] == 'linux'):
+		elif(platform == 'linux'):
 			# get file directory
 			path = path[0:path.rfind('/')];
 			# cd directory and start a terminal
 			command = 'cd ' + path + ' && gnome-terminal'
 			os.system(command)
 		else:
-			self.view.insert(edit, 0, 'Sorry, this plugin doestn\'t support your system now')
-			# self.view.insert(edit, 0, platform)
+			sublime.error_message('Sorry, this plugin doestn\'t support your system now. :(')
 
 
 		# dubug info
-		# self.view.insert(edit, 0, platform)
+		# sublime.error_message(platform)
